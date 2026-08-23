08331924184f0b9bde32b3cd19a1e2b184cf9c66
Relrige <91017454+Relrige@users.noreply.github.com>
2026-08-23 14:35:06 +0300

    Add WeatherService

---
+++ src/main/java/com/example/WeatherService.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import retrofit2.Call;
+import retrofit2.http.*;
+
+public interface WeatherService {
+    @GET("forecast.json")
+    Call<WeatherResponse> getForecast(
+            @Query("key") String apiKey,
+            @Query("q") String city,
+            @Query("days") int days
+    );
+}
